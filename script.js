16dc1f3bc511a7d73fac2375cef9821f64467696
bodie101 <keysbw@g.toiohomai.ac.nz>
2017-09-04 12:00:28 +1200

    problem02 done

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+let timestables = prompt("what number to divide", "00")
+
+for (let i = 0; i < 12; i++) {
+
+    timestables = parseInt(timestables)
+
+    let a = i + 1
+    console.log(a + " X "+ timestables + " = "+ (a/timestables) )
+}
+
+
